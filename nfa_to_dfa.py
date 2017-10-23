f5cd9a1d28fd6b5487c02ccdd91d99e34336144b
Servss <A01371719@itesm.mx>
2017-10-23 18:34:18 -0500

    Creación de la tabla de DFA y escritura a archivo de la tabla

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -1,3 +1,11 @@
+'''
+Jorge Alexis Rubio Sumano A01372074
+Servio Tulio Reyes Castillo A01371719
+
+Program which transforms a transformation from a NFA to a DFA
+
+'''
+
 from itertools import chain, combinations
 
 def powerset(iterable):
@@ -25,33 +33,58 @@ def obtener_nodos(valores):
     return lista_nodos
 
 def main():
-    ultima_linea = False
     todos_nodos = []
     conjunto_nodos = set([])
     conjunto_simbolos = set([])
-    while not ultima_linea:
-        valores = input()
-        if valores[-1] == "}":
-            ultima_linea = True
-            valores = valores[:-1]
 
-        valores = valores[1:]
-        todos_nodos = obtener_nodos(valores)
-        for nodo in todos_nodos:
-            #Agrega los nodos y los simbolos del conjunto
-            conjunto_simbolos.add(nodo[0])
-            conjunto_nodos.add(nodo[1])
-            conjunto_nodos.add(nodo[2])
+    #Creacion de archivos
+    input_file_name = input("Enter the file name to use as NFA input: ")
+    output_file_name = input("Enter the file name to save the DFA output: ")
+    valores = ""
+    file = open(input_file_name, "r")   
+    for line in file.readlines():
+        valores += line;
+    file.close()
 
-        combinaciones = list(powerset(conjunto_nodos))
-        print("Conjuntos", end="")
-        simbolos = list(conjunto_simbolos)
+    file = open(output_file_name, "w")
+
+    valores = valores[1:-1]
+    todos_nodos = obtener_nodos(valores)
+    for nodo in todos_nodos:
+        #Agrega los nodos y los simbolos del conjunto
+        conjunto_simbolos.add(nodo[0])
+        conjunto_nodos.add(nodo[1])
+        conjunto_nodos.add(nodo[2])
+
+    combinaciones = list(powerset(conjunto_nodos))
+    #print("Conjuntos", end="")
+    file.write("Conjuntos")
+    simbolos = list(conjunto_simbolos)
+    for simbolo in simbolos:
+        #print("\t"*(len(conjunto_nodos)//2+1), end="")
+        #print(simbolo, end="")
+        file.write("\t"*(len(conjunto_nodos)//2+1))
+        file.write(simbolo)
+    #print()
+    file.write("\n")
+    for combinacion in combinaciones:
+        #print(str(combinacion)+("\t"*((len(conjunto_nodos)//2+1)-(len(combinacion)//2)+1)), end='')
+        file.write(str(combinacion)+("\t"*((len(conjunto_nodos)//2+1)-(len(combinacion)//2)+1)))
         for simbolo in simbolos:
-            print("\t"*(len(conjunto_nodos)//2), end="")
-            print(simbolo, end="")
-        print()
-        for combinacion in combinaciones:
-            print(combinacion)
-        
+            conjunto_parcial = set([])
+            for nodo in todos_nodos:
+                if nodo[0] == simbolo and nodo[1] in combinacion:
+                    conjunto_parcial.add(nodo[2])
+            if len(conjunto_parcial) == 0:
+                #print("{}", end='')
+                file.write("{}")
+            else:
+                #print(conjunto_parcial, end='')
+                file.write(str(conjunto_parcial))
+            #print("\t"*((len(conjunto_nodos)//2+1)-(len(conjunto_parcial)//2)), end='')
+            file.write("\t"*((len(conjunto_nodos)//2+1)-(len(conjunto_parcial)//2)))
+        #print()
+        file.write("\n")
+    file.close()
 
 main()
