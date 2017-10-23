7b0f81b521dab234c50642296db5c4773f60624e
AlexisRubio96 <jarss96@hotmail.com>
2017-10-23 10:53:04 -0500

    Estados de transicion(falta corregir)

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -6,7 +6,6 @@ def powerset(iterable):
     return chain.from_iterable(combinations(xs,n) for n in range(len(xs)+1))
 
 def obtener_nodos(valores):
-    print(valores)
     #Obtiene los nodos del string ingresado
     lista_nodos = []
     cuenta_inicio = 0
@@ -23,14 +22,44 @@ def obtener_nodos(valores):
             lista.append(temporal[1])
             lista.append(temporal[2])
             lista_nodos.append(lista)
+    for element in lista_nodos:
+        print(element)
     return lista_nodos
 
+def obtener_transicion(combinacion, simbolo):
+    global todos_nodos
+    #Un solo estado
+    if(len(combinacion) == 1):
+        for transicion in todos_nodos:
+            if(transicion[0]==simbolo and transicion[1]==combinacion[0]):
+                print("estado transicion con ", simbolo, "= ", transicion[2])
+
+
+    else:
+        lista_nodos = [];
+        lista_transiciones = []
+        for estado in combinacion:
+            for transicion in todos_nodos:
+                if(transicion[0]==simbolo and estado==transicion[1]):
+                    lista_transiciones.append(transicion[2])
+
+        print("estado transicion con ", simbolo, "= ", lista_transiciones)
+
+    return
+
+
+
+#Variables globales
+todos_nodos = []
+estados_transicion = []
+
 def main():
+    global todos_nodos
     archivo_NFA = open(input());
     automatas = archivo_NFA.read().split("\n")
+    #Procesamiento de cada NFA
     for nfa in automatas:
         ultima_linea = False
-        todos_nodos = []
         conjunto_nodos = set([])
         conjunto_simbolos = set([])
         valores = nfa
@@ -41,21 +70,23 @@ def main():
 
             valores = valores[1:]
             todos_nodos = obtener_nodos(valores)
+            print(todos_nodos, "¨¨¨¨¨¨¨¨¨¨¨¨¨¨")
             for nodo in todos_nodos:
                 #Agrega los nodos y los simbolos del conjunto
                 conjunto_simbolos.add(nodo[0])
                 conjunto_nodos.add(nodo[1])
                 conjunto_nodos.add(nodo[2])
 
-            combinaciones = list(powerset(conjunto_nodos))
             print("Conjuntos", end="")
             simbolos = list(conjunto_simbolos)
             for simbolo in simbolos:
                 print("\t"*(len(conjunto_nodos)//2), end="")
                 print(simbolo, end="")
-            print()
+            combinaciones = list(powerset(conjunto_nodos))
             for combinacion in combinaciones:
                 print(combinacion)
+                for simbolo in conjunto_simbolos:
+                    obtener_transicion(combinacion, simbolo)
         
 
 main()
